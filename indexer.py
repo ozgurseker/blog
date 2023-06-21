27d64b54d4b050bc5048ab701968a14e3637b661
ozgurseker <ozgursheker@gmail.com>
2023-06-21 15:35:12 -0400

    a

--- indexer.py
+++ indexer.py
@@ -1,9 +1,16 @@
 import os
 folder = "posts/"
 filelist = os.listdir(folder)
+indexed_list = ""
+base_url = "https://ozgurseker.github.io/blog/"
+folder_url = base_url + folder
 if "index.md" in filelist:
     for name in [x for x in filelist if x.endswith('.md')]:
         if name == "index.md":
-            
+            None
         else:
+            page_url = folder_url + name[:-3]
+            indexed_list = indexed_list + "[" + name[:-3] + "](" + page_url + ") \n \n"
             
+    with open(folder + "index.md", 'w') as f:
+        f.write(indexed_list)
---
+++ posts/index.md
@@ -0,0 +1,8 @@
+[Jupyter Notebook to Post Test](https://ozgurseker.github.io/blog/posts/Jupyter Notebook to Post Test) 
+ 
+[Media Test](https://ozgurseker.github.io/blog/posts/Media Test) 
+ 
+[test](https://ozgurseker.github.io/blog/posts/test) 
+ 
+[First Blog Post](https://ozgurseker.github.io/blog/posts/First Blog Post) 
+ 
